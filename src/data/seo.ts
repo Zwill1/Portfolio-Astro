15dc94dc152619f278035beeda1384b7435a7636
zwill <zwill_1@hotmail.com>
2025-06-04 23:09:16 -0600

    adding SEO data folder and file to project

---
+++ src/data/seo.ts
@@ -0,0 +1,14 @@
+export const seoData = {
+  '/': {
+    title: 'Visuals Plus | Vernon, Kelowna and Calgary Web Design and Development | BC Web Design',
+    description: 'Visuals Plus is a Vernon, Kelowna and Calgary Web Design and Development business promoting local businesses with web page design, graphic design, online media',
+  },
+  '/portfolio': {
+    title: 'Portfolio | Visuals Plus',
+    description: 'Check out my some of my work in progress and completed project as I continue my full stack developer journey.',
+  },
+  '/education': {
+    title: 'Education | Visuals Plus',
+    description: 'Learn more about my education and experience! Mark is a Graphic and multimedia graduate, studying in web development and design based in Calgary, Alberta',
+  },
+};

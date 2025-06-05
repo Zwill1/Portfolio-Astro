7dc08f7ce07fb4d8e1ae0d1f09541707eae36499
zwill <zwill_1@hotmail.com>
2025-06-05 00:06:58 -0600

    changing base back for further testing on failed builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     integrations: [icon(), react()],
     vite: {    
-        plugins: [tailwindcss()],  
+        plugins: [tailwindcss()], 
     },
 //       image: {
 //     service: {

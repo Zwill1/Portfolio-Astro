49db9dc93b222a3975760872389ed02f190209a9
zwill <zwill_1@hotmail.com>
2025-06-06 23:10:46 -0600

    updating package to add partytown for 3rd party scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
     // No adapter needed for static sites!
     output: 'static', // optional, since static is the default
     site: 'https://visualsplus.com/',
-    integrations: [icon(), react(), sitemap()],
+    integrations: [icon(), react(), sitemap(), partytown()],
     vite: {    
         plugins: [tailwindcss()],
     },

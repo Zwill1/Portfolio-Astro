6bb027135b444e1a1271f4f2366b11f3a51c349a
zwill <zwill_1@hotmail.com>
2025-07-05 19:26:03 -0600

    updating timeline file for props to receive an object and map over it

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -2,16 +2,11 @@ interface TimeLineProps {
     jobTitle: string;
     jobDates: string;
     jobTags: string;
-    list1: string;
-    list2: string;
-    list3: string;
-    list4: string;
-    list5: string;
-    list6: string;
-    list7: string;
+    listItems: string[];
 }
 
-const TimeLine: React.FC<TimeLineProps> = ({jobTitle, jobDates, jobTags, list1, list2, list3, list4, list5, list6, list7}) => {
+const TimeLine: React.FC<TimeLineProps> = ({jobTitle, jobDates, jobTags, listItems}) => {
+  console.log(listItems);
   return (
     <>
         <div className="w-full md:w-5/12 lg:w-3/12 p-4 m-4 bg-[#f7f7f7] rounded-3xl drop-shadow-xl">
@@ -20,13 +15,9 @@ const TimeLine: React.FC<TimeLineProps> = ({jobTitle, jobDates, jobTags, list1, 
             <p className="pb-2 text-sm"><em>{jobTags}</em></p>
             <p className="mb-2 font-semibold">Job Duties:</p>
             <ul>
-                <li><i className="ri-arrow-right-double-line"></i> {list1}</li>
-                <li><i className="ri-arrow-right-double-line"></i> {list2}</li>
-                <li><i className="ri-arrow-right-double-line"></i> {list3}</li>
-                <li><i className="ri-arrow-right-double-line"></i> {list4}</li>
-                <li><i className="ri-arrow-right-double-line"></i> {list5}</li>
-                <li><i className="ri-arrow-right-double-line"></i> {list6}</li>
-                <li><i className="ri-arrow-right-double-line"></i> {list7}</li>
+                {listItems?.map((item, index) => (
+                  <li key={index}><i className="ri-arrow-right-double-line"></i> {item}</li>
+                ))}                
             </ul>
         </div>
     </>

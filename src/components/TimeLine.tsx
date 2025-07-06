9e1d049ec525b25a6b09eb6b11e9a7d34a6981c3
zwill <zwill_1@hotmail.com>
2025-07-05 19:52:34 -0600

    changing props to accept object for job tags. Adding job start and end props.

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -1,18 +1,22 @@
 interface TimeLineProps {
     jobTitle: string;
-    jobDates: string;
-    jobTags: string;
+    jobStart: string;
+    jobEnd: string;
+    jobTags: string[];
     listItems: string[];
 }
 
-const TimeLine: React.FC<TimeLineProps> = ({jobTitle, jobDates, jobTags, listItems}) => {
-  console.log(listItems);
+const TimeLine: React.FC<TimeLineProps> = ({jobTitle, jobStart, jobEnd, jobTags, listItems}) => {
   return (
     <>
         <div className="w-full md:w-5/12 lg:w-3/12 p-4 m-4 bg-[#f7f7f7] rounded-3xl drop-shadow-xl">
             <h4 className="text-lg font-bold uppercase leading-[1.5] lg:text-left">{jobTitle}</h4>
-            <p className="pb-2 text-sm font-bold text-[#17A2B8]"><em>{jobDates}</em></p>
-            <p className="pb-2 text-sm"><em>{jobTags}</em></p>
+            <p className="pb-2 text-sm font-bold text-[#17A2B8]"><em>{jobStart} - {jobEnd}</em></p>
+            <p className="pb-2 text-sm">
+                {jobTags?.map((tag, index) => (
+                  <em key={index}>{tag}, </em>
+                ))}    
+            </p>
             <p className="mb-2 font-semibold">Job Duties:</p>
             <ul>
                 {listItems?.map((item, index) => (
